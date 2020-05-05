3fefe648c029db1bc79b9d4a8635efa03fc3a06f
Dominator <amitsamant@outlook.com>
2020-05-05 23:11:15 +0530

    Tried to convert Any to String

--- Sources/ElegantAPI/Extensions/URLComponents+.swift
+++ Sources/ElegantAPI/Extensions/URLComponents+.swift
@@ -14,8 +14,8 @@ extension URLComponents{
         self.queryItems = self.queryItems ?? [] + parameters.map {
             if $0.1 is String{
                 return URLQueryItem(name: $0.0, value: $0.1 as? String)
-            }else if $0.1 is LosslessStringConvertible{
-                return URLQueryItem(name: String($0.0), value: String($0.1 as! LosslessStringConvertible))
+            }else{
+                return URLQueryItem(name: String($0.0), value: String(describing: $0.1))
             }
         }
         .compactMap{$0}
